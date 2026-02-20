0a429a9379543b370aa55381fbe1c38403854a3b
jose-lopez <josesmooth@gmail.com>
2026-02-20 13:39:08 -0400

    updating the installation from github

--- grecy/__main__.py
+++ grecy/__main__.py
@@ -1,27 +1,28 @@
 import typer
 import os
 from grecy.connections import access_to
-from grecy.__version__ import __version__
 
 app = typer.Typer()
 
 @app.command()
 def install(model: str):
 
-    models = ["grc_perseus_sm","grc_perseus_lg","grc_perseus_trf","grc_proiel_sm","grc_proiel_lg","grc_proiel_trf"]
+    models = ["grc_perseus_sm","grc_perseus_lg","grc_perseus_trf","grc_proiel_sm","grc_proiel_lg","grc_proiel_trf", "grc_ner_trf"]
 
-    hf_url = "https://huggingface.co/"
+    git_url = "https://github.com/jmyerston/greCy/releases/download/v3.7.5/"
+    git_release_url_to_check = "https://github.com/jmyerston/greCy/releases/tag/v3.7.5"
 
     if model in models:
 
         # Checking the access to Hugging Face
-        if not access_to(hf_url):
-            print(f'The access to {hf_url} is not possible.')
+        if not access_to(git_release_url_to_check):
+            print(f'The access to {git_release_url_to_check} is not possible.')
             print(f'Please, check the network connection.')
             exit(0)
 
         # The url for the model
-        https = hf_url + "Jacobo/" + model + "/resolve/main/" + model + "-" + __version__ + "-py3-none-any.whl"
+        # https = hf_url + "Jacobo/" + model + "/resolve/main/" + model + "-any-py3-none-any.whl"
+        https = git_url + model + "-3.7.5-py3-none-any.whl"
 
         # The pip command
         pip_command = "python -m pip install " + https
@@ -43,7 +44,7 @@ def install(model: str):
 @app.command()
 def uninstall(model: str):
 
-    models = ["grc_perseus_sm","grc_perseus_lg","grc_perseus_trf","grc_proiel_sm","grc_proiel_lg","grc_proiel_trf"]
+    models = ["grc_perseus_sm","grc_perseus_lg","grc_perseus_trf","grc_proiel_sm","grc_proiel_lg","grc_proiel_trf", "grc_ner_trf"]
 
     if model in models:
 
